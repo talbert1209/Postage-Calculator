d590b3b39c1d5119da201210f0626e01adde86bc
talbert1209 <talbert1209@gmail.com>
2018-12-12 15:42:10 -0600

    Completed challenge.

---
+++ Postage Calculator/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Postage_Calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Postage Calculator</h1>
+            <br />
+            <br />
+            Width:
+            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Height:
+            <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Length:
+            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" GroupName="shippingType" OnCheckedChanged="groundRadioButton_CheckedChanged" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="shippingType" OnCheckedChanged="airRadioButton_CheckedChanged" Text="Air" />
+            <br />
+            <asp:RadioButton ID="nextDayRadioButton" runat="server" AutoPostBack="True" GroupName="shippingType" OnCheckedChanged="nextDayRadioButton_CheckedChanged" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Postage Calculator/Default.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Postage_Calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = DisplayResult();
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = DisplayResult();
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = DisplayResult();
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = DisplayResult();
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = DisplayResult();
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = DisplayResult();
+        }
+
+        // Start of helper methods
+
+        private double GetVolume(double height, double width, double length = 1)
+        {
+            return height * width * length;
+        }
+
+        private double GetMultiplier()
+        {
+            if (groundRadioButton.Checked)
+                return .15;
+            if (airRadioButton.Checked)
+                return .25;
+            else
+                return .45;
+        }
+
+        private bool IsMinInfoProvided()
+        {
+            if (!string.IsNullOrWhiteSpace(widthTextBox.Text) && !string.IsNullOrWhiteSpace(heightTextBox.Text) &&
+                (groundRadioButton.Checked || airRadioButton.Checked || nextDayRadioButton.Checked)) return true;
+
+            return false;
+        }
+
+        private bool VerifyTextBoxContents(string textBoxText)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxText))
+                return true;
+            if (double.TryParse(textBoxText, out var dimension))
+                return true;
+            return false;
+        }
+
+        public double CalculateShipping()
+        {
+            var multiplier = GetMultiplier();
+
+            if (string.IsNullOrWhiteSpace(lengthTextBox.Text))
+                return multiplier * GetVolume(double.Parse(heightTextBox.Text), double.Parse(widthTextBox.Text));
+            return multiplier * GetVolume(double.Parse(heightTextBox.Text), double.Parse(widthTextBox.Text), double.Parse(lengthTextBox.Text));
+        }
+
+        private string DisplayResult()
+        {
+            if (IsMinInfoProvided())
+            {
+                // First verify that the entered values are valid doubles
+                if (!VerifyTextBoxContents(widthTextBox.Text.Trim()))
+                    return "Please enter a valid value for width!";
+                if (!VerifyTextBoxContents(heightTextBox.Text.Trim()))
+                    return "Please enter a valid value for height!";
+                if (!VerifyTextBoxContents(lengthTextBox.Text.Trim()))
+                    return "please enter a valid value for length!";
+
+                return $"Mailing this package will cost {CalculateShipping():C}";
+            }
+
+            return "Please enter all required information!";
+        }
+    }
+}
